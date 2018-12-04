c5c8d4aeb7afef0cb624fc825fc4353ab8b904ea
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-12-04 11:49:41 -0800

    split up unit tests better

--- src/test/unit/effects.js
+++ src/test/unit/effects.js
@@ -1,11 +1,7 @@
 const Stream = require('stream');
-const crypto = require('crypto');
 const tape = require('tape');
 
-const P5 = require('@code-dot-org/p5');
-
 const danceParty = require('../../danceParty');
-const replay = require('../fixtures/replay.json');
 const { renderImages } = require('../../replayToMovie');
 
 const BG_EFFECTS = [
@@ -150,34 +146,4 @@ tape('foreground effects can all be consumed without breaking canvas', (t) => {
       renderImages(replay, writableStream);
     });
   });
-});
-
-tape('replay log consistently renders to the same thing', (t) => {
-  t.plan(1);
-
-  // Seed p5 so randomized effects behave consistently
-  const p5Inst = new P5();
-  p5Inst.randomSeed(12345);
-
-  // Dump the output of renderImages directly to a hash
-  const writableStream = new Stream;
-  writableStream.writable = true;
-  const hash = crypto.createHash('sha1');
-  hash.setEncoding('hex');
-  writableStream.pipe(hash);
-  writableStream.write = function (newBuffer) {
-    hash.write(newBuffer);
-  };
-
-  writableStream.end = function (newBuffer) {
-    if (arguments.length) {
-      writableStream.write(newBuffer);
-    }
-
-    hash.end();
-    const shaSum = hash.read();
-    t.equals(shaSum, "6241d37ce069ec97aef2c694f6885b83c7167b4f");
-  };
-
-  renderImages(replay, writableStream);
 });
